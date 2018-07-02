791ed14681455bf14f281d0965a3ad66c2301684
John Barker <jebarker@gmail.com>
2018-07-02 15:47:06 -0700

    add spec digest into create spec API response
    Signed-off-by: Steve Ellis <email@steveell.is>

--- internal/cltest/cltest.go
+++ internal/cltest/cltest.go
@@ -247,8 +247,9 @@ func NewJobSubscriber() (*store.Store, services.JobSubscriber, func()) {
 
 // CommonJSON has an ID, and Name
 type CommonJSON struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Digest string `json:"digest"`
 }
 
 // ParseCommonJSON will unmarshall given body into CommonJSON

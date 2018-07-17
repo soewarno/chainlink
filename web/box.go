b0ab00c3857f57a388e73b1935738546853d2b0c
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-07-17 08:38:08 -0700

    Unit test gui routing match functions

--- web/box.go
+++ web/box.go
@@ -1,12 +1,56 @@
-// +build !test
-
 package web
 
 import (
-	"github.com/gobuffalo/packr"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
-// NewBox returns production build artifacts when the test tag is not present
-func NewBox() packr.Box {
-	return packr.NewBox("../gui/dist/")
+func MatchWildcardBoxPath(boxList []string, path string, file string) (matchedPath string) {
+	idPattern := regexp.MustCompile(`(_[a-zA-Z0-9]+_)`)
+	pathSeparator := (string)(os.PathSeparator)
+	escapedPathSeparator := pathSeparator
+	if pathSeparator == `\` {
+		escapedPathSeparator = `\\`
+	}
+	pathAndFile := filepath.Clean(strings.Join(
+		[]string{path, file},
+		pathSeparator,
+	))
+	normalizedPathAndFile := strings.Replace(
+		strings.TrimPrefix(pathAndFile, `/`),
+		`/`,
+		pathSeparator,
+		-1,
+	)
+
+	for i := 0; i < len(boxList) && matchedPath == ""; i++ {
+		boxPathWithIDPattern := idPattern.ReplaceAllString(boxList[i], `[a-zA-Z0-9]+`)
+		pathPattern := fmt.Sprintf(
+			`^%s$`,
+			strings.Replace(boxPathWithIDPattern, `\`, escapedPathSeparator, -1),
+		)
+		match, _ := regexp.MatchString(pathPattern, normalizedPathAndFile)
+
+		if match {
+			matchedPath = boxList[i]
+		}
+	}
+
+	return matchedPath
+}
+
+func MatchExactBoxPath(boxList []string, path string) (matchedPath string) {
+	pathWithoutPrefix := strings.TrimPrefix(path, "/")
+
+	for i := 0; i < len(boxList) && matchedPath == ""; i++ {
+		boxPath := boxList[i]
+		if boxPath == pathWithoutPrefix {
+			matchedPath = boxPath
+		}
+	}
+
+	return matchedPath
 }
